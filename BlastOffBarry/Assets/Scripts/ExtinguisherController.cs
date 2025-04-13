65168c25ceee8f7f99f1f09879da34bcd6efcf70
Abhinandan <abhimehta1324@gmail.com>
2025-04-13 16:14:31 +0200

    sprite assets added

--- BlastOffBarry/Assets/Scripts/ExtinguisherController.cs
+++ BlastOffBarry/Assets/Scripts/ExtinguisherController.cs
@@ -2,21 +2,37 @@ using UnityEngine;
 
 public class ExtinguisherController : MonoBehaviour
 {
-    public float offsetDistance = 0.4f;
+    public Transform extinguisher;
+    public SpriteRenderer barryRenderer;
+    public float horizontalOffset = 0.5f;
+    public Vector3 extinguisherSize = new Vector3(1f, 1f, 1f);
 
     void Update()
     {
-        if (Camera.main == null) return;
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = (mouseWorld - transform.position).normalized;
 
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 playerPos = transform.parent.position;
+        // Flip Barry and rotate extinguisher accordingly
+        if (dir.x < 0)
+        {
+            barryRenderer.flipX = true;
+            extinguisher.localScale = new Vector3(-extinguisherSize.x, extinguisherSize.y, extinguisherSize.z);
 
-        Vector3 direction = (mousePos - playerPos).normalized;
+            // Flip arc direction on left side
+            float angle = Mathf.Atan2(dir.y, -dir.x) * Mathf.Rad2Deg;
+            extinguisher.rotation = Quaternion.Euler(0f, 0f, angle);
+        }
+        else
+        {
+            barryRenderer.flipX = false;
+            extinguisher.localScale = extinguisherSize;
 
-        Vector3 offset = direction * offsetDistance;
-        transform.localPosition = offset;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            extinguisher.rotation = Quaternion.Euler(0f, 0f, angle);
+        }
 
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        // Keep extinguisher at horizontal offset, allow some vertical if needed
+        float xOffset = Mathf.Sign(dir.x) * horizontalOffset;
+        extinguisher.localPosition = new Vector3(xOffset, 0f, 0f);
     }
 }
