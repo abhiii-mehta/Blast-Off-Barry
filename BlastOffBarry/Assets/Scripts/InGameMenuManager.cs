d919e0d470fdea63273573415379acbabffb2872
Abhinandan <abhimehta1324@gmail.com>
2025-04-15 05:27:13 +0200

    canvas set to screen size

--- BlastOffBarry/Assets/Scripts/InGameMenuManager.cs
+++ BlastOffBarry/Assets/Scripts/InGameMenuManager.cs
@@ -5,6 +5,33 @@ public class InGameMenuManager : MonoBehaviour
 {
     public GameObject gameOverPanel;
     public GameObject victoryPanel;
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) ResumeGame();
+            else PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+
 
     public void ShowGameOver()
     {
@@ -21,7 +48,8 @@ public class InGameMenuManager : MonoBehaviour
     public void RestartGame()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
     }
 
     public void ReturnToMainMenu()
